e2e1d9c524c1b60a6e34e1852ee76f80fed384f6
atchimaphon <atchimaphon@users.noreply.github.com>
2019-09-22 14:39:50 +0700

    closewindown

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
   newgame = () => {
     window.location.reload(false);
   }
+  close = () =>{
+    window.close(true);
+  }
   render() {
     return (
       <div className = "App" >
@@ -25,6 +28,7 @@ class App extends Component {
         <h2 id="result"></h2>
         <h2 id="again">You can try again 5 time</h2>
         <button id="newgame" className="button" onClick={this.newgame}>NEW GAME</button>
+        <button id="close" className="button" onClick={this.close}>EXIT</button>
         <div id ="media">
         <video controls autostart autoPlay src={Logo} type="logo/mp4" />
          <h4>Atchimaphon Supharat 6010110399</h4>
